d0c2341485655ca910fd160d6f43ac098994d64d
Jungels_Aiden <junga526@hsd.k12.or.us>
2024-03-01 15:31:43 -0800

    worked on the main menu text

--- AJ Videogame GD Root/Scripts/change_scene.gd
+++ AJ Videogame GD Root/Scripts/change_scene.gd
@@ -11,5 +11,5 @@ func _process(_delta):
 		get_tree().change_scene_to_file("res://Scenes/level_1.tscn")
 	if Input.is_action_pressed("sc2"):
 		get_tree().change_scene_to_file("res://Scenes/level_2.tscn")
-	if Input.is_action_pressed("sc3"):
+	if Input.is_action_pressed("sc0"):
 		get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
--- AJ Videogame GD Root/Scripts/rotate_menu.gd
+++ AJ Videogame GD Root/Scripts/rotate_menu.gd
@@ -7,5 +7,5 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	rotate(Vector3(0,1,0), 0.001)
