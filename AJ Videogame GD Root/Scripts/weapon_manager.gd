bb606185a987104560db7d634c7bd9a28fcd2680
Jungels_Aiden <junga526@hsd.k12.or.us>
2024-04-08 14:02:46 -0700

    continued work on weapons manager

--- AJ Videogame GD Root/Scripts/weapon_manager.gd
+++ AJ Videogame GD Root/Scripts/weapon_manager.gd
@@ -8,3 +8,32 @@ var weapon_list = {}
 
 @export var _weapon_resources: Array[weapons_resource]
 @export var start_weapons: Array[String]
+
+func _ready():
+	initialize(start_weapons)
+
+
+func initialize(_start_weapons: Array):
+	#Enter into the state machine
+	for weapon in _weapon_resources:
+		weapon_list[weapon.weapon_name] = weapon
+	
+	for i in start_weapons:
+		weapon_stack.push_back(i)
+	
+	current_weapon = weapon_list[weapon_stack[0]]
+	enter()
+
+
+func enter():
+	#Call when you switch to a weapon
+	pass
+
+
+func change_weapon():
+	pass
+
+
+func exit():
+	#In order to change weapons call exit first
+	pass
