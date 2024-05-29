4be5e927d88e45e7f656e7cdbe27425b12db68e8
Jungels_Aiden <junga526@hsd.k12.or.us>
2024-05-29 14:51:16 -0700

    fixed crash with grenade, but it still wont apply any forces for some reason

--- AJ Videogame GD Root/Scripts/potato_bullet.gd
+++ AJ Videogame GD Root/Scripts/potato_bullet.gd
@@ -7,9 +7,10 @@ var damage: int = 0
 func _on_body_entered(body):
 	if body.is_in_group("target") && body.has_method("hit_successful"):
 		body.hit_successful(damage)
-	for target in explode_rad.get_overlapping_bodies():
-		if target is RigidBody3D or CharacterBody3D:
-			target.apply_central_force((target.global_transform.origin - self.global_transform.origin).normalized() * 100)
+	if body.is_in_group("grenade"):
+		for target in explode_rad.get_overlapping_bodies():
+			if target is RigidBody3D or CharacterBody3D:
+				target.apply_force((target.global_transform.origin - self.global_transform.origin).normalized() * 500)
 	queue_free()
 
 
