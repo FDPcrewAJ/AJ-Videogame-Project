c0d52076686684e31b3a0a02ef2b48f1ab7aa299
Jungels_Aiden <junga526@hsd.k12.or.us>
2024-05-29 10:26:16 -0700

    started working on explosion physics

--- AJ Videogame GD Root/Scripts/potato_bullet.gd
+++ AJ Videogame GD Root/Scripts/potato_bullet.gd
@@ -1,12 +1,15 @@
 extends RigidBody3D
 
 var damage: int = 0
+@onready var explode_rad = $Area3D
 
 
 func _on_body_entered(body):
 	if body.is_in_group("target") && body.has_method("hit_successful"):
 		body.hit_successful(damage)
-		queue_free()
+	for target in explode_rad.get_overlapping_bodies():
+		if target is RigidBody3D or CharacterBody3D:
+			target.apply_central_force((target.global_transform.origin - self.global_transform.origin).normalized() * 100)
 	queue_free()
 
 
