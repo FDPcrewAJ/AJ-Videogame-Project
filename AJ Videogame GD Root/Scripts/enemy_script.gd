5fcfdbacc813d53476ae28bdac24fe3fcc98cba1
Aiden <junga526@hsd.k12.or.us>
2024-05-28 21:20:45 -0700

    added crosses over eyes when enemies dies

--- AJ Videogame GD Root/Scripts/enemy_script.gd
+++ AJ Videogame GD Root/Scripts/enemy_script.gd
@@ -1,6 +1,8 @@
 extends RigidBody3D
 
 var health = 5
+@onready var timer = get_node("%Death Timer")
+@onready var death_crosses = get_node("%eye_cross_container")
 
 func hit_successful(damage, _direction:= Vector3.ZERO, _position:= Vector3.ZERO):
 	var hit_position = _position - get_global_transform().origin
@@ -8,7 +10,12 @@ func hit_successful(damage, _direction:= Vector3.ZERO, _position:= Vector3.ZERO)
 	health -= damage
 	print("Enemy Health = " + str(health))
 	if health <= 0:
-		queue_free()
+		timer.start()
+		death_crosses.visible = true
 	
 	if _direction != Vector3.ZERO:
 		apply_impulse((_direction * damage), hit_position)
+
+
+func _on_death_timer_timeout():
+	queue_free()
