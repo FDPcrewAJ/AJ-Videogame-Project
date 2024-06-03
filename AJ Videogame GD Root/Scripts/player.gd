66273a51a9196d1f0284d93dbee0c2f57c977cfb
Jungels_Aiden <junga526@hsd.k12.or.us>
2024-06-03 13:45:34 -0700

    added a death screen

--- AJ Videogame GD Root/Scripts/player.gd
+++ AJ Videogame GD Root/Scripts/player.gd
@@ -8,6 +8,10 @@ class_name Player
 @onready var collision_check = $collision_check
 @onready var camera_3d = $neck/head/player_camera
 @onready var grapplecast = $neck/head/grapple_cast
+@onready var death_menu = $death_menu
+
+# Varible to disable inputs when dead
+var allow_control = true
 
 # Speed and Movement Variables
 var current_speed = 5.0
@@ -59,14 +63,15 @@ func _ready():
 
 func _input(event):
 	# Mouse Looking Logic
-	if event is InputEventMouseMotion:
-		if free_looking:
-			neck.rotate_y(deg_to_rad(-event.relative.x * mouse_sens))
-			neck.rotation.y = clamp(neck.rotation.y,deg_to_rad(-100),deg_to_rad(100))
-		else:
-			rotate_y(deg_to_rad(-event.relative.x * mouse_sens))
-			head.rotate_x(deg_to_rad(-event.relative.y * mouse_sens))
-			head.rotation.x = clamp(head.rotation.x,deg_to_rad(-89),deg_to_rad(89))
+	if allow_control:
+		if event is InputEventMouseMotion:
+			if free_looking:
+				neck.rotate_y(deg_to_rad(-event.relative.x * mouse_sens))
+				neck.rotation.y = clamp(neck.rotation.y,deg_to_rad(-100),deg_to_rad(100))
+			else:
+				rotate_y(deg_to_rad(-event.relative.x * mouse_sens))
+				head.rotate_x(deg_to_rad(-event.relative.y * mouse_sens))
+				head.rotation.x = clamp(head.rotation.x,deg_to_rad(-89),deg_to_rad(89))
 
 
 func grapple():
@@ -140,103 +145,119 @@ func look_for_point():
 func _physics_process(delta):
 	# Reset Scene when key is pressed, and Change scenes when the key is pressed.
 	# Get movement inputs
-	var input_dir = Input.get_vector("left", "right", "forward", "backward")
-	# Handling Movement States
-	if Input.is_action_pressed("crouch") || sliding:
-		# Crouching
-		current_speed = lerp(current_speed, crouching_speed, delta * lerp_speed)
-		head.position.y = lerp(head.position.y, crouch_depth, delta * lerp_speed)
-		
-		standing_collision_shape.disabled = true
-		crouching_collision_shape.disabled = false
-		
-		# Slide Begin Logic
-		if sprinting && input_dir != Vector2.ZERO:
-			sliding = true
-			slide_timer = slide_timer_max
-			slide_vector = input_dir
-			free_looking = true
-		
-		walking = false
-		sprinting = false
-		crouching = true
-	elif !collision_check.is_colliding():
-		# Standing
-		standing_collision_shape.disabled = false
-		crouching_collision_shape.disabled = true
-		
-		head.position.y = lerp(head.position.y, 0.0, delta * lerp_speed)
-		if Input.is_action_pressed("sprint"):
-			# Sprinting
-			current_speed = lerp(current_speed, sprinting_speed, delta * lerp_speed)
+	if allow_control:
+		var input_dir = Input.get_vector("left", "right", "forward", "backward")
+		# Handling Movement States
+		if Input.is_action_pressed("crouch") || sliding:
+			# Crouching
+			current_speed = lerp(current_speed, crouching_speed, delta * lerp_speed)
+			head.position.y = lerp(head.position.y, crouch_depth, delta * lerp_speed)
+			
+			standing_collision_shape.disabled = true
+			crouching_collision_shape.disabled = false
+			
+			# Slide Begin Logic
+			if sprinting && input_dir != Vector2.ZERO:
+				sliding = true
+				slide_timer = slide_timer_max
+				slide_vector = input_dir
+				free_looking = true
+			
 			walking = false
-			sprinting = true
-			crouching = false
-		else:
-			# Walking
-			current_speed = lerp(current_speed, walking_speed, delta * lerp_speed)
-			walking = true
 			sprinting = false
-			crouching = false
+			crouching = true
+		elif !collision_check.is_colliding():
+			# Standing
+			standing_collision_shape.disabled = false
+			crouching_collision_shape.disabled = true
+			
+			head.position.y = lerp(head.position.y, 0.0, delta * lerp_speed)
+			if Input.is_action_pressed("sprint"):
+				# Sprinting
+				current_speed = lerp(current_speed, sprinting_speed, delta * lerp_speed)
+				walking = false
+				sprinting = true
+				crouching = false
+			else:
+				# Walking
+				current_speed = lerp(current_speed, walking_speed, delta * lerp_speed)
+				walking = true
+				sprinting = false
+				crouching = false
 
-	# Handle Free Look
-	if Input.is_action_pressed("free_look") || sliding:
-		free_looking = true
-		camera_3d.rotation.z = -deg_to_rad(neck.rotation.y * free_look_tilt_amount)
-	else:
-		free_looking = false
-		neck.rotation.y = lerp(neck.rotation.y, 0.0, delta * lerp_speed)
-		camera_3d.rotation.z = lerp(camera_3d.rotation.z, 0.0, delta * lerp_speed)
-	
-	# Handle Sliding
-	if sliding:
-		slide_timer -= delta
-		if slide_timer <= 0:
-			sliding = false
+		# Handle Free Look
+		if Input.is_action_pressed("free_look") || sliding:
+			free_looking = true
+			camera_3d.rotation.z = -deg_to_rad(neck.rotation.y * free_look_tilt_amount)
+		else:
 			free_looking = false
-
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y -= gravity * delta
-
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = jump_velocity
-		sliding = false
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	if is_on_floor():
-		direction = lerp(direction,(transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized(), delta * lerp_speed)
-	else:
-		direction = lerp(direction,(transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized(), delta * air_lerp_speed)
+			neck.rotation.y = lerp(neck.rotation.y, 0.0, delta * lerp_speed)
+			camera_3d.rotation.z = lerp(camera_3d.rotation.z, 0.0, delta * lerp_speed)
 		
-	if sliding:
-		direction = (transform.basis * Vector3(slide_vector.x, 0, slide_vector.y)).normalized()
-		current_speed = (slide_timer + 0.1) * slide_speed
+		# Handle Sliding
+		if sliding:
+			slide_timer -= delta
+			if slide_timer <= 0:
+				sliding = false
+				free_looking = false
 
-	if direction:
-		velocity.x = direction.x * current_speed
-		velocity.z = direction.z * current_speed
-
-	else:
-		velocity.x = move_toward(velocity.x, 0, current_speed)
-		velocity.z = move_toward(velocity.z, 0, current_speed)
-	if Input.is_action_pressed("load_checkpoint"):
-		global_position = singleton.check_point_pos
+		# Add the gravity.
+		if not is_on_floor():
+			velocity.y -= gravity * delta
 
 
-	#print(grapple_joint.global_position)
-	#print(grapple_point)
-	check_hook_activation()
+		# Handle jump.
+		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+			velocity.y = jump_velocity
+			sliding = false
+
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom gameplay actions.
+		if is_on_floor():
+			direction = lerp(direction,(transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized(), delta * lerp_speed)
+		else:
+			direction = lerp(direction,(transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized(), delta * air_lerp_speed)
+			
+		if sliding:
+			direction = (transform.basis * Vector3(slide_vector.x, 0, slide_vector.y)).normalized()
+			current_speed = (slide_timer + 0.1) * slide_speed
+
+		if direction:
+			velocity.x = direction.x * current_speed
+			velocity.z = direction.z * current_speed
+
+		else:
+			velocity.x = move_toward(velocity.x, 0, current_speed)
+			velocity.z = move_toward(velocity.z, 0, current_speed)
+		if Input.is_action_pressed("load_checkpoint"):
+			global_position = singleton.check_point_pos
 	
+	check_hook_activation()
 	move_and_slide()
 
 
-func _on_death_detector_area_entered(_area):
-	global_position = singleton.check_point_pos
-
-
 func _on_weapon_manager_use_grapple():
-	grapple()
+	if allow_control:
+		grapple()
+
+
+func _on_death_detector_area_entered(_area):
+	death()
+	allow_control = false
+	velocity = Vector3.ZERO
+
+
+func death():
+	death_menu.show()
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+
+func _on_load_chpt_button_pressed():
+	global_position = singleton.check_point_pos
+	allow_control = true
+	death_menu.hide()
+	_ready()
+
+
+func _on_restart_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
